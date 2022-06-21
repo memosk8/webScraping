d050aec7f7aba6397ca95a2830fd5b77338ecee8
memosk8 <memolopez9363@gmail.com>
2022-06-21 10:14:57 -0500

    git init. working on objects to pollute the DB

--- scraping.py
+++ scraping.py
@@ -1,30 +1,48 @@
-from time import sleep
-import requests
 from bs4 import BeautifulSoup
-import pymongo
+import requests
 
-html = requests.get('https://tunein.com/radio/music/').text
-soup = BeautifulSoup(html, 'html.parser')
+html_doc = requests.get('https://tunein.com/radio/music/').text
+soup = BeautifulSoup(html_doc, 'lxml')
 
-main_containers = soup.select('.container-items-module__containerItem___OhnxW')
-title_class = "container-title-module__titleHeader___WUX8D"
-item_class_row = 'gallery-module__item___lU4KU'
-item_class_col = 'row guide-item-module__guideItemContainer___I50sj'
-item_classes = ['gallery-module__item___lU4KU', 'guide-item-module__guideItemContainer___I50sj',
-                'link-module__container___vhfuW', 'numbered-link-module__infoContainerWrapper___JIB6D', ]
-count = 0
-for item in main_containers:
-    count = count + 1
-    print(count)
-    print("Sección :", item.find('div', title_class).text)
-    if(item.find('div', item_class_row)):
-        print("Contenido :", item.find('div', item_class_row).text)
-    if(item.find('div', item_class_row)):
-        print("Contenido :", item.find('div', item_class_col))
-    if(item.find('div', 'link-module__container___vhfuW')):
-        print("Contenido :", item.find(
-            'div', 'link-module__container___vhfuW').text)
-    if(item.find('div', 'numbered-link-module__infoContainerWrapper___JIB6D')):
-        print("Contenido :", item.find(
-            'div', 'numbered-link-module__infoContainerWrapper___JIB6D').text)
-    sleep(0.2)
+page_title = soup.title
+section_titles = soup.find_all('div','container-title-module__titleHeader___WUX8D')
+
+# obtener secciones completas
+main_containers = soup.find_all('div', 'container-items-module__containerItem___OhnxW')
+#clases de subelementos de todas las secciones
+elements_classes = ['guide-item-module__guideItemTitleMultiLine___ddgqh guide-item-module__guideItemTitle___nYoaH',
+						  'numbered-link-module__headerText___PPhv6',
+						  'guide-item-module__guideItemTitleAlt___oAxuc']
+
+sections = []
+elements = []
+i = 0;
+# crear un objeto por cada seccion {titulo: [elementos]}
+for container in main_containers:
+	#print('\t',container.text,'\n')
+	#titulo de la seccion
+	title = container.find('div', 'container-title-module__titleHeader___WUX8D')
+	#elementos de la seccion
+	for element in elements_classes:
+		if(container.find('div',element)):
+			elements.append(container.find('div',element).text)
+		#content = container.find_all('div','')
+
+	sections.append({title.text: elements})
+	print(sections[i],'\n')
+	i = i + 1
+# section# = {section_title: section_elements}
+
+# todos los elementos individuales
+# content_classes = soup.find_all('div','guide-item-module__guideItemTitleMultiLine___ddgqh guide-item-module__guideItemTitle___nYoaH')
+# content_classes.extend(soup.find_all('div','numbered-link-module__infoContainerWrapper___JIB6D'))
+# print(len(content_classes))
+# content_classes.extend(soup.find_all('div','titles-module__titleText___KQtb_'))
+# print(len(content_classes))
+# for item in content_classes:
+#	print(item.text)
+
+#Get all the links on <a> tags
+#for link in soup.find_all('a'):
+	#print(link.get('href'))
+
