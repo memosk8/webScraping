0453c07e8f4ed921b809c43501a361644c6c2069
memosk8 <memolopez9363@gmail.com>
2022-06-26 19:21:57 -0500

    - Generates obj for each category:     {title, content} - Saves categories to mongodb

--- scraping.py
+++ scraping.py
@@ -1,49 +1,62 @@
+import string
 from bs4 import BeautifulSoup
 import requests
+import pymongo
 
-html_doc = requests.get('https://tunein.com/radio/music/').text
-soup = BeautifulSoup(html_doc, 'lxml')
+html_doc = requests.get('https://tunein.com/radio/music/')
+soup = BeautifulSoup(html_doc.content, 'lxml')
 
+# <title/>
 page_title = soup.title
-section_titles = soup.find_all('div','container-title-module__titleHeader___WUX8D')
 
-# obtener secciones completas
-main_containers = soup.find_all('div', 'container-items-module__containerItem___OhnxW')
-#clases de subelementos de todas las secciones
-elements_classes = ['guide-item-module__guideItemTitleMultiLine___ddgqh',
-					'numbered-link-module__numberedLinkContainer___EPfHi',
-					'link-module__container___vhfuW', 'titles-module__titleText___KQtb_']
+# titulos de categorias
+section_titles = soup.find_all(
+    'div', 'container-title-module__titleHeader___WUX8D')
+title_class = 'container-title-module__titleHeader___WUX8D'
 
-sections = []
-elements = []
-i = 0
-# crear un objeto por cada seccion {titulo: [elementos]}
-for container in main_containers:
-	#titulo de la seccion
-	title = section_titles[i]
-	#elementos de la seccion
-	print(f"#{i} title: {title.text} ")
-	for element in elements_classes:
-		if(container.find('div',element)):
-			elements.append(container.find('div',element))
-			print(element)
-	i = i + 1
+# obtener secciones completas (titulo, contenido)
+category_containers = soup.find_all(
+    'div', 'container-items-module__containerItem___OhnxW')
 
-#sections.append({title.text: elements})
-#print(sections[i],'\n')
-#i = i + 1
-# section# = {section_title: section_elements}
+# clases de contenido de subelementos de todas las secciones
+subcontain_classes = ['guide-item-module__guideItemTitleMultiLine___ddgqh guide-item-module__guideItemTitle___nYoaH',
+                      'numbered-link-module__numberedLinkContainer___EPfHi',
+                      'link-module__container___vhfuW', 'numbered-link-module__headerText___PPhv6', 'titles-module__titleText___KQtb_']
 
-# todos los elementos individuales
-# content_classes = soup.find_all('div','guide-item-module__guideItemTitleMultiLine___ddgqh guide-item-module__guideItemTitle___nYoaH')
-# content_classes.extend(soup.find_all('div','numbered-link-module__infoContainerWrapper___JIB6D'))
-# print(len(content_classes))
-# content_classes.extend(soup.find_all('div','titles-module__titleText___KQtb_'))
-# print(len(content_classes))
-# for item in content_classes:
-#	print(item.text)
+categories = []
+for container in category_containers:
+    section = {}
 
-#Get all the links on <a> tags
-#for link in soup.find_all('a'):
-	#print(link.get('href'))
+    # titulo de la seccion
+    if(container.find('div', 'container-title-module__titleHeader___WUX8D')):
+        section['title'] = container.find(
+            'div', 'container-title-module__titleHeader___WUX8D').text
+        section['content'] = []
+    else:
+        break
 
+    for subclass in subcontain_classes:
+        # si existe al menos un elemento en la seccion
+        if(container.find('div', subclass)):
+            # contenido de la seccion
+            elements = container.find_all('div', subclass)
+            for elem in elements:
+                # añadir el texto de cada elemento a la lista de contenido de la seccion
+                section['content'].append(elem.text)
+    # añade seccion a lista de categorias
+    categories.append(section)
+
+# categoria sin texto solo imagenes
+categories.pop(2)
+# local conection string
+# pymongo.MongoClient('mongodb://127.0.0.1') 
+# cloud conection string
+client = pymongo.MongoClient('mongodb+srv://memosk8:Password1232@cluster0.nuhhssx.mongodb.net/?retryWrites=true&w=majority')
+if(client):
+    print("\n-- Mongo connected --\n")
+scrap = client.tunein.categories
+try:
+    scrap.insert_many(categories)
+    print(f'se insertaron {len(categories)} citas')
+except pymongo.errors.ConnectionFailure as error:
+    print(error)
